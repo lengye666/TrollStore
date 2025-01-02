ae36526f2788a312fa50727511b7541554698454
lengye666 <1750297226@qq.com>
2025-01-02 17:20:30 +0800

    Delete Exploits/fastPathSign directory

--- Exploits/fastPathSign/src/codesign.h
+++
@@ -1,4 +0,0 @@
-#import <stdbool.h>
-#import <Foundation/Foundation.h>
-
-int codesign_sign_adhoc(const char *path, bool preserveMetadata, NSDictionary *customEntitlements);
--- Exploits/fastPathSign/src/coretrust_bug.h
+++
@@ -1,1 +0,0 @@
-int apply_coretrust_bypass(const char *machoPath);
