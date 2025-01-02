968dc8fe0ba4a0c8f4ea20abc6d3e8532fb4a92a
lengye666 <1750297226@qq.com>
2025-01-02 21:46:36 +0800

    Update unarchive.m

--- RootHelper/unarchive.m
+++ RootHelper/unarchive.m
@@ -1,87 +1,49 @@
 #import "unarchive.h"
-
-#include <archive.h>
-#include <archive_entry.h>
-
-static int
-copy_data(struct archive *ar, struct archive *aw)
-{
-  int r;
-  const void *buff;
-  size_t size;
-  la_int64_t offset;
-
-  for (;;) {
-    r = archive_read_data_block(ar, &buff, &size, &offset);
-    if (r == ARCHIVE_EOF)
-      return (ARCHIVE_OK);
-    if (r < ARCHIVE_OK)
-      return (r);
-    r = archive_write_data_block(aw, buff, size, offset);
-    if (r < ARCHIVE_OK) {
-      fprintf(stderr, "%s\n", archive_error_string(aw));
-      return (r);
-    }
-  }
-}
+#import <Foundation/Foundation.h>
 
 int extract(NSString* fileToExtract, NSString* extractionPath)
 {
-    struct archive *a;
-    struct archive *ext;
-    struct archive_entry *entry;
-    int flags;
-    int r;
-
-    /* Select which attributes we want to restore. */
-    flags = ARCHIVE_EXTRACT_TIME;
-    flags |= ARCHIVE_EXTRACT_PERM;
-    flags |= ARCHIVE_EXTRACT_ACL;
-    flags |= ARCHIVE_EXTRACT_FFLAGS;
-
-    a = archive_read_new();
-    archive_read_support_format_all(a);
-    archive_read_support_filter_all(a);
-    ext = archive_write_disk_new();
-    archive_write_disk_set_options(ext, flags);
-    archive_write_disk_set_standard_lookup(ext);
-    if ((r = archive_read_open_filename(a, fileToExtract.fileSystemRepresentation, 10240)))
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    NSError *error = nil;
+    
+    // 创建目标目录
+    if (![fileManager createDirectoryAtPath:extractionPath
+            withIntermediateDirectories:YES
+            attributes:nil
+            error:&error]) {
+        NSLog(@"Failed to create directory: %@", error);
         return 1;
-    for (;;)
-    {
-        r = archive_read_next_header(a, &entry);
-        if (r == ARCHIVE_EOF)
-            break;
-        if (r < ARCHIVE_OK)
-            fprintf(stderr, "%s\n", archive_error_string(a));
-        if (r < ARCHIVE_WARN)
-            return 1;
-        
-        NSString* currentFile = [NSString stringWithUTF8String:archive_entry_pathname(entry)];
-        NSString* fullOutputPath = [extractionPath stringByAppendingPathComponent:currentFile];
-        //printf("extracting %@ to %@\n", currentFile, fullOutputPath);
-        archive_entry_set_pathname(entry, fullOutputPath.fileSystemRepresentation);
-        
-        r = archive_write_header(ext, entry);
-        if (r < ARCHIVE_OK)
-            fprintf(stderr, "%s\n", archive_error_string(ext));
-        else if (archive_entry_size(entry) > 0) {
-            r = copy_data(a, ext);
-            if (r < ARCHIVE_OK)
-                fprintf(stderr, "%s\n", archive_error_string(ext));
-            if (r < ARCHIVE_WARN)
-                return 1;
-        }
-        r = archive_write_finish_entry(ext);
-        if (r < ARCHIVE_OK)
-            fprintf(stderr, "%s\n", archive_error_string(ext));
-        if (r < ARCHIVE_WARN)
-            return 1;
     }
-    archive_read_close(a);
-    archive_read_free(a);
-    archive_write_close(ext);
-    archive_write_free(ext);
+    
+    // 根据文件扩展名选择解压方法
+    NSString *extension = [fileToExtract.lowercaseString pathExtension];
+    
+    if ([extension isEqualToString:@"zip"]) {
+        // 使用系统自带的 zip 解压功能
+        NSTask *task = [[NSTask alloc] init];
+        task.launchPath = @"/usr/bin/unzip";
+        task.arguments = @[@"-o", fileToExtract, @"-d", extractionPath];
+        
+        [task launch];
+        [task waitUntilExit];
+        
+        return task.terminationStatus;
+    }
+    else if ([extension isEqualToString:@"tar"]) {
+        // 使用系统自带的 tar 解压功能
+        NSTask *task = [[NSTask alloc] init];
+        task.launchPath = @"/usr/bin/tar";
+        task.arguments = @[@"-xf", fileToExtract, @"-C", extractionPath];
+        
+        [task launch];
+        [task waitUntilExit];
+        
+        return task.terminationStatus;
+    }
+    else {
+        NSLog(@"Unsupported archive format: %@", extension);
+        return 1;
+    }
     
     return 0;
 }
