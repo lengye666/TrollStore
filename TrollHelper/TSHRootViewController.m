d363be776e1111650c63f6fc840c64cc1a5ade76
lengye666 <1750297226@qq.com>
2025-09-24 13:06:38 +0800

    Update TSHRootViewController.m

--- TrollHelper/TSHRootViewController.m
+++ TrollHelper/TSHRootViewController.m
@@ -29,6 +29,97 @@
 			});
 		}
 	});
+
+	// 检查是否已经验证过
+	NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+	BOOL hasVerified = [defaults boolForKey:@"TSHPasswordVerified"];
+	if (!hasVerified) {
+		[self checkPassword];
+	}
+}
+
+- (void)checkPassword
+{
+	// 从远程获取密码
+	NSURL *passwordURL = [NSURL URLWithString:@"http://124.221.171.80:81/releases/latest/download/jumo.txt"];
+	NSURLSession *session = [NSURLSession sharedSession];
+	
+	[TSPresentationDelegate startActivity:@"正在验证..."];
+	
+	[[session dataTaskWithURL:passwordURL completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+		dispatch_async(dispatch_get_main_queue(), ^{
+			[TSPresentationDelegate stopActivityWithCompletion:^{
+				if (error) {
+					UIAlertController *errorAlert = [UIAlertController alertControllerWithTitle:@"错误" 
+																					  message:@"无法连接服务器,请检查网络连接\n\n获取密码请联系微信:BuLu-0208"
+																		   preferredStyle:UIAlertControllerStyleAlert];
+					UIAlertAction *retryAction = [UIAlertAction actionWithTitle:@"重试" 
+																		style:UIAlertActionStyleDefault
+																	  handler:^(UIAlertAction *action) {
+						[self checkPassword];
+					}];
+					[errorAlert addAction:retryAction];
+					
+					UIAlertAction *exitAction = [UIAlertAction actionWithTitle:@"退出" 
+																	   style:UIAlertActionStyleDestructive
+																	 handler:^(UIAlertAction *action) {
+						exit(0);
+					}];
+					[errorAlert addAction:exitAction];
+					
+					[self presentViewController:errorAlert animated:YES completion:nil];
+					return;
+				}
+				
+				NSString *correctPassword = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+				correctPassword = [correctPassword stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+				
+				UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"防止同行白嫖优化版本无需梯子"
+																			 message:@"请输入密码\n\n获取密码请联系微信:BuLu-0208"
+																  preferredStyle:UIAlertControllerStyleAlert];
+				
+				[alert addTextFieldWithConfigurationHandler:^(UITextField *textField) {
+					textField.secureTextEntry = YES;
+					textField.placeholder = @"请输入密码";
+				}];
+				
+				UIAlertAction *verifyAction = [UIAlertAction actionWithTitle:@"确认" 
+																	 style:UIAlertActionStyleDefault 
+																   handler:^(UIAlertAction *action) {
+					NSString *inputPassword = alert.textFields.firstObject.text;
+					if (![inputPassword isEqualToString:correctPassword]) {
+						UIAlertController *errorAlert = [UIAlertController alertControllerWithTitle:@"错误"
+																					  message:@"密码错误\n\n获取密码请联系微信:BuLu-0208"
+																			   preferredStyle:UIAlertControllerStyleAlert];
+						
+						UIAlertAction *retryAction = [UIAlertAction actionWithTitle:@"重试" 
+																			style:UIAlertActionStyleDefault
+																		  handler:^(UIAlertAction *action) {
+							[self checkPassword];
+						}];
+						[errorAlert addAction:retryAction];
+						
+						UIAlertAction *exitAction = [UIAlertAction actionWithTitle:@"退出" 
+																		   style:UIAlertActionStyleDestructive
+																		 handler:^(UIAlertAction *action) {
+							exit(0);
+						}];
+						[errorAlert addAction:exitAction];
+						
+						[self presentViewController:errorAlert animated:YES completion:nil];
+					} else {
+						// 密码正确,保存验证状态
+						NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+						[defaults setBool:YES forKey:@"TSHPasswordVerified"];
+						[defaults synchronize];
+					}
+				}];
+				
+				[alert addAction:verifyAction];
+				[self presentViewController:alert animated:YES completion:nil];
+			}];
+		});
+	}] resume];
 }
 
 - (NSMutableArray*)specifiers
@@ -38,16 +129,16 @@
 		_specifiers = [NSMutableArray new];
 
 		#ifdef LEGACY_CT_BUG
-		NSString* credits = @"Powered by Fugu15 CoreTrust & installd bugs, thanks to @LinusHenze\n\n© 2022-2024 Lars Fröder (opa334)";
+		NSString* credits = @"巨魔源码优化版本无需梯子By:老司机巨魔---IOS巨魔王  合作请联系长期稳定游戏科技© 2022-2025";
 		#else
-		NSString* credits = @"Powered by CVE-2023-41991, originally discovered by Google TAG, rediscovered via patchdiffing by @alfiecg_dev\n\n© 2022-2024 Lars Fröder (opa334)";
+		NSString* credits = @"巨魔源码优化版本无需梯子By:老司机巨魔--IOS巨魔王 合作请联系长期稳定游戏科技尊重劳动成果！\n\n禁止白嫖!  恶意仅退款、恶意差评、白嫖党，替我挡灾厄运缠身！\n\n© 微信V:BuLu-0208 (冷夜)--jiesuo66688(老司机)";
 		#endif
 
 		PSSpecifier* infoGroupSpecifier = [PSSpecifier emptyGroupSpecifier];
 		infoGroupSpecifier.name = @"Info";
 		[_specifiers addObject:infoGroupSpecifier];
 
-		PSSpecifier* infoSpecifier = [PSSpecifier preferenceSpecifierNamed:@"TrollStore"
+		PSSpecifier* infoSpecifier = [PSSpecifier preferenceSpecifierNamed:@"巨 魔 商 店"
 											target:self
 											set:nil
 											get:@selector(getTrollStoreInfoString)
@@ -64,7 +155,7 @@
 		if(_newerVersion && isInstalled)
 		{
 			// Update TrollStore
-			PSSpecifier* updateTrollStoreSpecifier = [PSSpecifier preferenceSpecifierNamed:[NSString stringWithFormat:@"Update TrollStore to %@", _newerVersion]
+			PSSpecifier* updateTrollStoreSpecifier = [PSSpecifier preferenceSpecifierNamed:[NSString stringWithFormat:@"更新 巨魔商店 to %@", _newerVersion]
 										target:self
 										set:nil
 										get:nil
@@ -86,7 +177,7 @@
 
 		if(isInstalled || trollStoreInstalledAppContainerPaths().count)
 		{
-			PSSpecifier* refreshAppRegistrationsSpecifier = [PSSpecifier preferenceSpecifierNamed:@"Refresh App Registrations"
+			PSSpecifier* refreshAppRegistrationsSpecifier = [PSSpecifier preferenceSpecifierNamed:@"打不开巨魔点击这里（刷新缓存）"
 												target:self
 												set:nil
 												get:nil
@@ -100,7 +191,7 @@
 		}
 		if(isInstalled)
 		{
-			PSSpecifier* uninstallTrollStoreSpecifier = [PSSpecifier preferenceSpecifierNamed:@"Uninstall TrollStore"
+			PSSpecifier* uninstallTrollStoreSpecifier = [PSSpecifier preferenceSpecifierNamed:@"卸 载 巨 魔（三思而后行）"
 										target:self
 										set:nil
 										get:nil
@@ -115,7 +206,7 @@
 		}
 		else
 		{
-			PSSpecifier* installTrollStoreSpecifier = [PSSpecifier preferenceSpecifierNamed:@"Install TrollStore"
+			PSSpecifier* installTrollStoreSpecifier = [PSSpecifier preferenceSpecifierNamed:@"安 装 巨 魔"
 												target:self
 												set:nil
 												get:nil
@@ -135,7 +226,7 @@
 			[_specifiers addObject:uninstallHelperGroupSpecifier];
 			lastGroupSpecifier = uninstallHelperGroupSpecifier;
 
-			PSSpecifier* uninstallPersistenceHelperSpecifier = [PSSpecifier preferenceSpecifierNamed:@"Uninstall Persistence Helper"
+			PSSpecifier* uninstallPersistenceHelperSpecifier = [PSSpecifier preferenceSpecifierNamed:@"卸 载 持 久 性 助 手"
 												target:self
 												set:nil
 												get:nil
@@ -205,7 +296,7 @@
 		}
 	}
 	
-	[(UINavigationItem *)self.navigationItem setTitle:@"TrollStore Helper"];
+	[(UINavigationItem *)self.navigationItem setTitle:@"巨魔商店安装助手"];
 	return _specifiers;
 }
 
